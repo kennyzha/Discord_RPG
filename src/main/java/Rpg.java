b4fe5273b9a46c54c459448df7797143a5327dca
Kenny <kzhao7301@gmail.com>
2018-05-25 12:36:32 -0400

    able to receive messages from server and private messages

--- src/main/java/Rpg.java
+++ src/main/java/Rpg.java
@@ -1,5 +1,14 @@
-public class Rpg {
-    public static void main(String[] args){
+import listener.MessageListener;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
 
+import javax.security.auth.login.LoginException;
+
+public class Rpg {
+    public static void main(String[] args) throws LoginException, RateLimitedException, InterruptedException{
+        JDA jda = new JDABuilder(AccountType.BOT).setToken(Config.TOKEN).buildBlocking();
+        jda.addEventListener(new MessageListener());
     }
 }
---
+++ src/main/java/listener/MessageListener.java
@@ -0,0 +1,22 @@
+package listener;
+
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class MessageListener extends ListenerAdapter {
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.isFromType(ChannelType.PRIVATE))
+        {
+            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
+                    event.getMessage().getContentDisplay());
+        }
+        else
+        {
+            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
+                    event.getTextChannel().getName(), event.getMember().getEffectiveName(),
+                    event.getMessage().getContentDisplay());
+        }
+    }
+}
