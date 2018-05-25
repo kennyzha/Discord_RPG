31aa3573357866817ccc4136c33c07acc42eeade
Kenny <kzhao7301@gmail.com>
2018-05-25 12:20:08 -0400

    initial project setup

---
+++ src/main/java/model/User.java
@@ -0,0 +1,6 @@
+package model;
+
+public class User {
+    private int level, attack, defense, strength, woodcutting;
+    private int levelExp, attackExp, defenseExp, strengthExp, woodCuttingExp;
+}
