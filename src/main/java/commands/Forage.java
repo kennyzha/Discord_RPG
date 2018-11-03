85a6d73b2433f56e98e9adb3aafba2e2edf5cdea
Kenny <kzhao7301@gmail.com>
2018-11-03 13:35:05 -0400

    working on guild implementation

--- src/main/java/commands/Forage.java
+++ src/main/java/commands/Forage.java
@@ -113,7 +113,7 @@ public class Forage {
 
 
             if(accessoryCratesFound > 0){
-                crate(channel, new String[]{"r!crate", "accessory", Integer.toString(accessoryCratesFound)}, user, true);
+                Crate.CrateCommand(new String[]{"r!crate", "accessory", Integer.toString(accessoryCratesFound)}, channel, playerDatabase, user, messageHandler, true);
             }
         } catch(NumberFormatException e){
             msg = String.format("Please enter a a valid number. You can only forage 20 times a day. You have already foraged %s times today.", player.getForageAmount());
--- src/main/java/models/Guild.java
+++ src/main/java/models/Guild.java
@@ -1,4 +1,13 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Guild {
+    private String name;
+    private String ownerID;
+    private ArrayList<String> members;
+    private int gold;
+    private HashMap<String, Integer> bank;
+    private int level;
 }
