018b905d9dfc0e9f0a439e8b8f6e3bb5f2844513
Kenny <kzhao7301@gmail.com>
2018-11-04 18:37:23 -0500

    donator unit tests

---
+++ src/test/java/DonatorTests/DonatorTest.java
@@ -0,0 +1,56 @@
+package DonatorTests;
+
+import utils.Donator;
+import models.Player;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DonatorTest {
+    @Test
+    public void isNotADonatorTest(){
+        Player player = new Player("abc");
+        assertEquals(false, Donator.isDonator(player));
+    }
+
+    @Test
+    public void isADonatorTest(){
+        Player player = new Player("abc");
+
+        System.out.println(player.getDonatorEndTime());
+        Donator.applyDonatorPacks(player, 1);
+        System.out.println(player.getDonatorEndTime());
+
+        assertEquals(true, Donator.isDonator(player));
+    }
+
+    @Test
+    public void donatorDaysTest(){
+        Player player = new Player("abc");
+        Donator.applyDonatorPacks(player, 1);
+
+        System.out.println(Donator.isDonator(player));
+        assertEquals(31, Donator.getDonatorTimeDays(player), 1);
+    }
+
+    @Test
+    public void donatorDaysTest2(){
+        Player player = new Player("abc");
+        int numDonatorPacks = 3;
+        Donator.applyDonatorPacks(player, 3);
+
+        System.out.println(Donator.isDonator(player));
+        assertEquals(93, Donator.getDonatorTimeDays(player), 1);
+    }
+
+    @Test
+    public void donatorAlmostExpiredTest(){
+        Player player = new Player("abc");
+
+        long threeDays = 1000 * 60 * 60 * 24 * 9;
+        player.setDonatorEndTime(System.currentTimeMillis() + threeDays);
+
+        assertEquals(9, Donator.getDonatorTimeDays(player), 1);
+    }
+
+}
