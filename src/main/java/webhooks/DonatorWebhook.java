1d89cc14b5066e2eda7abad3bba3b22c1411770c
Kenny <kzhao7301@gmail.com>
2018-11-26 13:58:08 -0500

    implemented donator webhook

--- src/main/java/webhooks/DonatorWebhook.java
+++ src/main/java/webhooks/DonatorWebhook.java
@@ -7,12 +7,15 @@ import java.io.*;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.google.gson.Gson;
 import config.ApplicationConstants;
+import config.ItemConstants;
 import database.PlayerDatabase;
 import models.DonatorTransaction;
+import models.Inventory;
 import models.Player;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import utils.Donator;
 
 public class DonatorWebhook implements RequestStreamHandler {
     JSONParser parser = new JSONParser();
@@ -31,34 +34,37 @@ public class DonatorWebhook implements RequestStreamHandler {
                 System.out.println("hps: " + hps.toJSONString());
                 auth = (String) hps.get("Authorization");
 
-                if (authIsValid(auth)) {
-                    if (event.get("body") != null) {
-                        JSONObject body = (JSONObject)parser.parse((String)event.get("body"));
-
+                if (event.get("body") != null) {
+                    if (authIsValid(auth)) {
                         Gson gson = new Gson();
                         DonatorTransaction donator = gson.fromJson((String) event.get("body"), DonatorTransaction.class);
                         System.out.println(donator.toString());
 
                         PlayerDatabase playerDatabase = new PlayerDatabase();
                         if(donator.getBuyer_id() != null && !donator.getBuyer_id().equals("")){
-                            Player player = playerDatabase.grabPlayer(donator.getBuyer_id());
-
-                            System.out.println(player.toString());
-
                             if(donator.getStatus() != null){
+                                Player player = playerDatabase.grabPlayer(donator.getBuyer_id());
+                                System.out.println(player.toString());
+
                                 String donatorStatus = donator.getStatus();
                                 if(donatorStatus.equals("completed")){
-                                    System.out.println(donatorStatus);
+                                    Donator.applyDonatorPacks(player, 1);
+                                    player.addItem(ItemConstants.RESET_SCROLL.toString(), 2);
+                                    System.out.println("Applied DP to " + player.getId());
                                 } else if(donatorStatus.equals("reversed") || donatorStatus.equals("refunded")){
-                                    System.out.println(donatorStatus);
+                                    Donator.removeDonatorPacks(player, 1);
+                                    System.out.println("Removed DP from " + player.getId());
                                 }
+                                playerDatabase.insertPlayer(player);
                             }
                         }
-
+                    else {
+                        System.out.println("unauthorized: " + event.get("body"));
                     }
-                } else {
-                    System.out.println("unauthorized");
-                }
+                } else{
+                        System.out.println("wrong auth");
+                    }
+            }
             }
             System.out.println("auth: " + auth);
             System.out.println("payload: " + bodyString);
